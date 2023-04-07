b209ebbd8497b2669d1c07fd6b2f94a6f31fe7b0
Bachan-kubi <felixghagra@gmail.com>
2023-04-07 17:48:14 +0600

    project completed

--- index.js
+++ index.js
@@ -5,21 +5,55 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+// to send form data
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res)=>{
     res.send('new api created!');
 });
 
+app.get('/product', async(req, res)=>{
+    const query = {};
+    const products = await Product.find(query);
+    res.send(products);
+});
+
+app.get('/product/:id', async(req, res)=>{
+    const {id} = req.params;
+    const products = await Product.findById(id);
+    res.send(products);
+});
+
+app.put('/product/:id', async(req, res)=>{
+    const {id} = req.params;
+    const products = await Product.findByIdAndUpdate(id, req.body);
+    if(!products){
+        return res.status(404).json({message: `coudlnt find any product from following ${id}`});
+    }
+    const updatedProduct= await Product.findById(id);
+    res.status(200).json(updatedProduct);
+});
+
+app.delete('/product/:id', async(req, res)=>{
+    const {id} = req.params;
+    const products = await Product.findByIdAndDelete(id, req.body);
+    if(!products){
+        return res.status(404).json({message: `coudlnt find any product from following ${id}`});
+    }
+    
+    res.status(200).json(products);
+});
+
 
 app.post('/product', async (req, res)=>{
-    console.log(req.body);
-    res.send(req.body);
-    // try{
-    //     const product = await Product.create(req.body);
-        
-    // }catch(err){
-    //     console.log(err)
-    // }
+    
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+        console.log(product);
+    }catch(err){
+        console.log(err)
+    }
 })
 app.get('/about', (req, res)=>{
     res.send('This is about!');
@@ -29,7 +63,7 @@ app.get('/blog', (req, res)=>{
     res.send('this is blog');
 });
 
-mongoose.connect('mongodb+srv://admin:GGJYHUnOZikqsURc@cluster0.3vga5dj.mongodb.net/node-api?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://admin:GGJYHUnOZikqsURc@cluster0.3vga5dj.mongodb.net/api-node?retryWrites=true&w=majority')
 .then(()=>{
     console.log('MongoDB with Mongoose connected!');
     app.listen(port, ()=>console.log(`api is running on ${port}`));
